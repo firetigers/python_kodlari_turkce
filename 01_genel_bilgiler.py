e08abeecbd0aacaa8837c9b9b938cd8e157079a1
firetigers <117107965+firetigers@users.noreply.github.com>
2023-12-11 11:24:43 +0300

    Python kodlari

--- 01_genel_bilgiler.py
+++ 01_genel_bilgiler.py
@@ -299,7 +299,8 @@
 # .sort()
 # .pop()
 
-# .append() # listelerin sonuna eleman eklemek için kullanılır, eklenen elemanı hep son sıraya atar. append fonksiyonu içine tek değer yazılmalıdır, aksi takdirde hata verecektir. 
+# .append() # listelerin sonuna eleman eklemek için kullanılır, eklenen elemanı hep son sıraya atar. 
+# Append fonksiyonu içine tek değer yazılmalıdır, aksi takdirde hata verecektir. 
 # bu özellik striglerle yani str.append() şeklinde asla kullanılmaz.
 # empty_list =[]
 # empty_list.append(1) 
